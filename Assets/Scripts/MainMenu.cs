f0a95f96cdc8d2de0c86a748c77c10a561c9d9bc
Michael S <kragerles1@gmail.com>
2025-10-06 16:56:16 -0400

    Unity Update

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // syntax of functions:
+    // public - access modifier, means this function can be accessed from other classes
+    // ReturnType FunctionName(ParameterType parameterName, ...)  {... function body ... }
+    // void means the function does not return any value
+    // StartGame is the name of the function
+    // () means the function does not take any parameters
+    // {...} is the body of the function
+    public void StartGame()
+    {
+        // SceneManager is an API from Unity that allows us to manage scenes
+        // LoadScene is a function that loads a scene by its name or index
+        // "Game" is the name of the scene we want to load
+        SceneManager.LoadScene("Game");
+    }
+}
